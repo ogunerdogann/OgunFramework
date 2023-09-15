c1f7f8bef234008988c424b7928a363237008c0e
hayyam2395 <ogun.erdogan23@gmail.com>
2023-09-16 01:28:52 +0200

    css control

--- src/test/java/step_definitions/example2_SD.java
+++ src/test/java/step_definitions/example2_SD.java
@@ -3,13 +3,15 @@ package step_definitions;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import lombok.SneakyThrows;
+import org.junit.Assert;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
+import javax.swing.*;
 import java.time.Duration;
 
-import static hooks.Hooks.commonPage;
-import static hooks.Hooks.driver;
+import static hooks.Hooks.*;
 
 public class example2_SD {
 
@@ -28,6 +30,21 @@ public class example2_SD {
         // Bu özellikleri web sitesinde görmek icin ise inspect yaptiktan sonra sag kisimda
         // Elements ve onun da alt kismindaki Component veya Style (component daha sade görünüme sahip)
         //  sekmesinden görebiliyoruz
+        Assert.assertEquals(
+                "rgba(245, 245, 250, 1)",
+                commonPage.getHomePage().silver_getStarted.getCssValue("background-color"));
+        Assert.assertEquals(
+                "rgba(23, 150, 95, 1)",
+                commonPage.getHomePage().silver_getStarted.getCssValue("color"));
+
+        // imlecle üzerine geldikten sonra arka plan rengi nasil degisiyor
+        hoverWebElement(commonPage.getHomePage().silver_getStarted);
+        waitFor(1);
+        Assert.assertEquals("rgba(23, 150, 95, 1)",
+                 commonPage.getHomePage().silver_getStarted.getCssValue("background-color"));
+        Assert.assertEquals("rgba(255, 255, 255, 1)",
+                commonPage.getHomePage().silver_getStarted.getCssValue("color"));
+
     }
 
     @Then("user should able to see gold background color and text color are expected value")
@@ -64,5 +81,9 @@ public class example2_SD {
        // }
     }
 
-
+        public void hoverWebElement(WebElement webElement){
+            actions.moveToElement(webElement).build().perform();
+            // Eger bir WebElement üzerinde Actions class'i yardimiyla birden fazla islem yapacaksak
+            //.build().perform() yapmaliyiz. Eger tek bir islem yapacaksak sadece .perform() yeterli olacaktir.
+        }
 }
